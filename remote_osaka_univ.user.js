d1c9a9d3e44cc52db0d6379a3cfb06a6580514ea
obameyan <obameyan29@gmail.com>
2020-04-25 22:41:42 +0900

    update

--- remote_osaka_univ.user.js
+++ remote_osaka_univ.user.js
@@ -11,6 +11,7 @@ function get_gateway_url(url) {
   return 'http://' + url_obj.hostname + '.remote.library.osaka-u.ac.jp' + url_obj.pathname + url_obj.search;
 }
 
+/*
 (function () {
   const links = Array.from(document.getElementsByTagName('a'));
   const re_url = new RegExp('^(https?|ftp):\/\/');
@@ -20,5 +21,30 @@ function get_gateway_url(url) {
   });
   journal_links.forEach(link => {
     link.href = get_gateway_url(link.href);
+  });
+})();
+*/
+
+function article_page_block(block) {
+  if ($(block).find("span.gs_ctc").length == 1) { //[PDF] or [HTML]
+    return false;
+  } else {
+    return true;
+  }
+}
+
+(function () {
+  const re_url = new RegExp('^(https?|ftp):\/\/');
+  const article_block = $('#gs_res_ccl_mid .gs_ri');
+  article_block.each((i, e) => {
+    if (article_page_block(e)) {
+      const journal_links = $(e).find("a").filter((i, link) => {
+        const href = link.href
+        return href.match(re_url) && (!href.match('google')) && link.hasAttribute("id");
+      });
+      journal_links.each((i, link) => {
+        link.href = get_gateway_url(link.href);
+      });
+    }
   });
 })();
