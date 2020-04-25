442a0355c970bcdae533e2275ceedfb9b0bb94ef
obameyan <obameyan29@gmail.com>
2020-04-26 04:35:21 +0900

    update

--- remote_osaka_univ.user.js
+++ remote_osaka_univ.user.js
@@ -3,9 +3,11 @@
 // @version  1
 // @include  https://scholar.google*
 // @grant    none
+// @require  https://code.jquery.com/jquery-3.5.0.slim.min.js
 // This software is released under the MIT License, see LICENSE.
 // ==/UserScript==
 
+/*
 function get_gateway_url(url) {
   const url_obj = new URL(url);
   return 'http://' + url_obj.hostname + '.remote.library.osaka-u.ac.jp' + url_obj.pathname + url_obj.search;
@@ -23,8 +25,18 @@ function get_gateway_url(url) {
     link.href = get_gateway_url(link.href);
   });
 })();
+*/
 
-/*
+function get_gateway_url(url) {
+  const url_obj = new URL(url);
+  if (url.match('arxiv')) {
+    return url;
+  } else {
+    return 'http://' + url_obj.hostname + '.remote.library.osaka-u.ac.jp' + url_obj.pathname + url_obj.search;
+  }
+}
+
+
 function article_page_block(block) {
   if ($(block).find("span.gs_ctc").length == 1) { //[PDF] or [HTML]
     return false;
@@ -48,4 +60,3 @@ function article_page_block(block) {
     }
   });
 })();
-*/
